3f01f55076b6f757321e20cd7f6858067193119f
Ninelle Cherp <2689344c@student.gla.ac.uk>
2023-02-10 17:15:57 +0000

    Finnal commit 2

--- rango/views.py
+++ rango/views.py
@@ -193,7 +193,8 @@ def visitor_cookie_handler(request):
     visits = int(get_server_side_cookie(request, 'visits', '1'))
     last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
-    if (datetime.now() - last_visit_time).seconds > 0:
+    
+    if (datetime.now() - last_visit_time).days > 0:
         visits += 1
         request.session['last_visit'] = str(datetime.now())
 
